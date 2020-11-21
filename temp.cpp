f14a7e89069c610a065a9626faa911eedfd3703c
repl.it user <replituser@example.com>
2020-11-21 06:42:20 +0000

    commitgit add .

--- temp.cpp
+++ temp.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"구름 IDE가 github랑 연동은 좋은데 못써먹것다..."<<endl;
+    cout<<"2020.11.21/ 15:39 구름 IDE가 github랑 연동은 좋은데 못써먹것다..."<<endl;
+    cout<<"2020.11.21/ 15:41 결국 다시 repl로 돌아온 부분..."<<endl;
     return 0;
 }
