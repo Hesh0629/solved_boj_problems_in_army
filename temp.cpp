c83e815e529d3eb2414dbc56495d9fbacf6eec09
한성환 <hesh0629@kakao.com>
2020-11-21 07:03:52 +0000

    쓰읍...

--- temp.cpp
+++ temp.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
     cout<<"2020.11.21/ 15:39 구름 IDE가 github랑 연동은 좋은데 못써먹것다..."<<endl;
     cout<<"2020.11.21/ 15:41 결국 다시 repl로 돌아온 부분..."<<endl;
+    cout<<"2020.11.21/ 16:03 구름 IDE님 잘못했습니다..."<<endl;
     return 0;
 }
