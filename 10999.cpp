28b595e88f201acc5073ffba18bbfdd5520b7a39
한성환 <hesh0629@kakao.com>
2020-11-29 05:35:54 +0000

    외않돼?

--- 10999.cpp
+++ 10999.cpp
@@ -23,38 +23,34 @@ int main(){
     segtree.resize(2*base+1);
     for(int i=0;i<n;i++){
         cin>>segtree[base+i].value;
-        segtree[base+i].x=i+1;
-        segtree[base+i].y=i+1;
+        segtree[base+i].x=i;
+        segtree[base+i].y=i;
         segtree[base+i].len=1;
         segtree[base+i].blank=0;
     }
     for(int i=base-1;i>=0;i--){
         segtree[i].value=segtree[2*i].value+segtree[2*i+1].value;
-        if(segtree[2*i].blank==1||segtree[2*i+1].blank==1){
-            if(segtree[2*i].blank==1&&segtree[2*i+1].blank==1){
-                segtree[base+i].blank=1;
-            }
-            else if(segtree[2*i].blank==1){
-                segtree[i].x=segtree[2*i+1].x;
-                segtree[i].y=segtree[2*i+1].y;
-                segtree[i].len=segtree[2*i+1].len;
-                segtree[base+i].blank=0;
-            }
-            else{
-                segtree[i].x=segtree[2*i].x;
-                segtree[i].y=segtree[2*i].y;
-                segtree[i].len=segtree[2*i].len;
-                segtree[base+i].blank=0;
-            }    
+        if(segtree[2*i].blank==1&&segtree[2*i+1].blank==1){
+            segtree[i].blank=1;
         }
-        else
+        else if(segtree[2*i].blank==1){
+            segtree[i].x=segtree[2*i+1].x;
+            segtree[i].y=segtree[2*i+1].y;
+            segtree[i].len=segtree[2*i+1].len;
+            segtree[i].blank=0;
+        }
+        else if(segtree[2*i+1].blank==1){
+            segtree[i].x=segtree[2*i].x;
+            segtree[i].y=segtree[2*i].y;
+            segtree[i].len=segtree[2*i].len;
+            segtree[i].blank=0;
+        }    
+        else{
             segtree[i].x=segtree[2*i].x;
             segtree[i].y=segtree[2*i+1].y;
             segtree[i].len=segtree[2*i].len+segtree[2*i+1].len;
-            segtree[base+i].blank=0;
-    }
-    for(int i=0;i<2*base+1;i++){
-        cout<<"len"<<i<<"="<<segtree[i].len<<endl;
+            segtree[i].blank=0;
+        }
     }
     for(int i=0;i<m+k;i++){
         int a,b,c,d;
@@ -66,25 +62,30 @@ int main(){
         else{
             cin>>b>>c;
             cout<<res(b-1,c-1)<<'\n';
+            
         }
     }
     return 0;
 }
-void add(int l,int r,int d,int index){
+void add(int l,int r,int d,int index){ //들어온 lazy를 노드 정보에 더해주는 함수
     if(segtree[index].len==1){
         segtree[index].value+=d;
     }
     else if(segtree[index].x==l&&segtree[index].y==r)
         segtree[index].lazy+=d;
     else{
-        int half=(segtree[index].x+segtree[index].y)/2;
-        if(half>=l)
+        int half=segtree[index*2].x+segtree[index*2].len-1;
+        if(r<=half)
+            add(l,r,d,2*index);
+        else if(l>=half+1)
+            add(l,r,d,2*index+1);
+        else{
             add(l,half,d,2*index);
-        if(half+1<=r)
             add(half+1,r,d,2*index+1);
+        }
     }
 }
-long long res(int l,int r){
+long long res(int l,int r){ //구간 합을 구하는 함수
     srch(l,r,1);
     long long ans=0;
     l+=base;
@@ -106,27 +107,35 @@ long long res(int l,int r){
     }
     return ans;
 }
-void srch(int l,int r,int index){
+void srch(int l,int r,int index){//구간합을 내기위해 탑 다운으로 가다가 laz를 만나면 propagate하며 업데이트 해주는 함수
     if(segtree[index].lazy!=0){
         propagate(index);
     }
     else{
         if(segtree[index].len!=1){
-            int half=(segtree[index].x+segtree[index].y)/2;
-            if(half>=l)
+            int half=segtree[index*2].x+segtree[index*2].len-1;
+            if(r<=half)
+                srch(l,r,2*index);
+            else if(l>=half+1)
+                 srch(l,r,2*index+1);
+            else{
                 srch(l,half,2*index);
-            if(half+1<=r)
-                srch(half+1,r,2*index+1);      
-        }
+                srch(half+1,r,2*index+1);
+            }
+            segtree[index].value=segtree[index*2].value+segtree[index*2+1].value;
+        }    
     }
 }
 void propagate(int index){
     segtree[index].value+=segtree[index].lazy*segtree[index].len;
-    segtree[index].lazy=0;
     if(segtree[index].len!=1){
         segtree[index*2].lazy=segtree[index].lazy;
         segtree[index*2+1].lazy=segtree[index].lazy;
+        segtree[index].lazy=0;
         propagate(index*2);
-        propagate(index*2+1);    
+        propagate(index*2+1);
+    }
+    else{
+        segtree[index].lazy=0;
     }
 }
