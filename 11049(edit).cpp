4f8230beb8b39b0742b3d779b2f686aa1dc5a060
HeSH <neogate@sogang.ac.kr>
2021-05-09 14:05:44 +0900

    Update 11049(edit).cpp

--- 11049(edit).cpp
+++ 11049(edit).cpp
@@ -1,24 +1,40 @@
+//파도파도 나오는 DP, memozation 문제
+//dp[s][e]에 [s,e] 구간의 최소값을 저장하고 분할 정복하는 방식.
+//이렇게 하면 기존 방식보단 편하게 풀 수 있었다.
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define INF 2147483646
 using namespace std;
+vector<pair<int,int>>mat;
+int dp[500][500]={0};
+int rec(int s,int e){
+	if(s>e)
+		return 0;
+	else if(s==e)
+		return 0;
+	else if(dp[s][e])
+		return dp[s][e];
+	else{
+		int res= INF;
+		for(int i=s;i<e;i++){
+			res=min(res, rec(s,i)+rec(i+1,e)+mat[s].first*mat[i].second*mat[e].second);
+		}
+		dp[s][e]=res; //memozation 진행
+		return res;
+	}
+}
 int main() {
 	int n;
 	cin>>n;
-	vector<pair<long long int,long long int>>mat;
 	for(int i=0;i<n;i++){
 		long long int r,c;
 		cin>>r>>c;
 		mat.push_back({r,c});
 	}
-	long long int dp[n][n];
-	for(int i=0;i<n-1;i++){
-		dp[0][i]=mat[i].first*mat[i].second*mat[i+1].second;
-	}
-	for(int i=1;i<n-1;i++){
-		for(int j=0;j<n-i-1;j++){
-			dp[i][j]=min(dp[i-1][j] + (mat[j].first * mat[i+1+j].first * mat[i+1+j].second), (mat[j].first * mat[j].second * mat[i+1+j].second) + dp[i-1][j+1]);
-		}
-	}
-	cout<<dp[n-2][0];
+	int ans = rec(0,n-1);
+	if(ans==INF)
+		cout<<0<<'\n';
+	else
+		cout<<ans<<'\n';
 }
