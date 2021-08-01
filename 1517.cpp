a535865bdf50b46a95ddda274ecc6c9d0adb6598
HeSH <neogate@sogang.ac.kr>
2021-08-01 20:17:46 +0900

    Update 1517.cpp

--- 1517.cpp
+++ 1517.cpp
@@ -6,17 +6,6 @@ using namespace std;
 long long int base,n;
 vector<long long int>tree;
 vector<pair<long long int,long long int>>input;
-long long int srch(int val){
-	long long int l=val+1,r=n-1;
-	long long int ans=0;
-	while(l<r){
-		if(l%2==1) ans+=tree[l++];
-		if(r%2==0) ans+=tree[r--];
-		l/=2; r/=2;
-	}
-	if(l==r) ans+=tree[l];
-	return ans;
-}
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
@@ -29,13 +18,13 @@ int main() {
 	  	cin>>init;
 	  	input.push_back({init,i});
   	}
-  	stable_sort(input.begin(),input.end());
+  	stable_sort(input.begin(),input.end(),greater<pair<long long int,long long int>>());
   	for(int i=0;i<n-1;i++){
 		if(input[i].first==input[i+1].first) input[i+1].second=input[i].second;
 	}
 	long long int ans=0;
   	for(int i=0;i<n;i++){
-		long long int l=base+input[i].second+1,r=base+n-1;
+		long long int l=base,r=base+input[i].second-1;
 		while(l<r){
 			if(l%2==1) ans+=tree[l++];
 			if(r%2==0) ans+=tree[r--];
