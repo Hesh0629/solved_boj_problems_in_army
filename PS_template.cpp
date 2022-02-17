0afbf5b277608877659300b538b403e423f2f299
HeSH <neogate@sogang.ac.kr>
2022-02-17 17:29:32 +0900

    Create PS_template.cpp

---
+++ PS_template.cpp
@@ -0,0 +1,16 @@
+// Template for PS
+// Created by HeSH on 2022.02.17
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <unordered_map>
+#define endl '\n'
+#define INF 1e13
+#define MAX 101010
+#define mod 1000000007
+using namespace std;
+typedef long long int ll;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+}
